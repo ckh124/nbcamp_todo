5ed68d27e2e649bf6eeee849e1efdab8085bd4fb
jess <sejin-011@hanmail.net>
2023-08-13 08:53:24 +0900

    enum을 data class로 변경

--- app/src/main/java/com/jess/camp/main/MainTabs.kt
+++ app/src/main/java/com/jess/camp/main/MainTabs.kt
@@ -1,14 +1,8 @@
 package com.jess.camp.main
 
 import androidx.fragment.app.Fragment
-import com.jess.camp.R
-import com.jess.camp.bookmark.BookmarkFragment
-import com.jess.camp.todo.TodoFragment
 
-enum class MainTabs(
+data class MainTabs(
     val fragment: Fragment,
     val titleRes: Int
-) {
-    TODO(TodoFragment.newInstance(), R.string.main_tab_todo_title),
-    BOOKMARK(BookmarkFragment.newInstance(), R.string.main_tab_bookmark_title)
-}
+)
--- app/src/main/java/com/jess/camp/main/MainViewPagerAdapter.kt
+++ app/src/main/java/com/jess/camp/main/MainViewPagerAdapter.kt
@@ -3,20 +3,33 @@ package com.jess.camp.main
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.jess.camp.R
+import com.jess.camp.todo.TodoFragment
 
 class MainViewPagerAdapter(
     fragmentActivity: FragmentActivity
 ) : FragmentStateAdapter(fragmentActivity) {
 
+    private val fragments = ArrayList<MainTabs>()
+
+    init {
+        fragments.add(
+            MainTabs(TodoFragment.newInstance(), R.string.main_tab_bookmark_title)
+        )
+        fragments.add(
+            MainTabs(TodoFragment.newInstance(), R.string.main_tab_todo_title),
+        )
+    }
+
     fun getTitle(position: Int): Int {
-        return MainTabs.values()[position].titleRes
+        return fragments[position].titleRes
     }
 
     override fun getItemCount(): Int {
-        return MainTabs.values().size
+        return fragments.size
     }
 
     override fun createFragment(position: Int): Fragment {
-        return MainTabs.values()[position].fragment
+        return fragments[position].fragment
     }
 }
